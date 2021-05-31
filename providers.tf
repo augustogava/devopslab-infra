3bc317c09459109958be345b0d307224cb47185c
Augusto Gava <augustodj@gmail.com>
2021-05-31 19:03:47 -0300

    Convertendo Credential

--- providers.tf
+++ providers.tf
@@ -10,7 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/vagrant/lab-devops-cloud-314722-843338b0f378.json")
 
   project = "lab-devops-cloud-314722"
   region  = "us-central1"
